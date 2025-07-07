13c0045c82d2fbad0aea54bb8c87b4895863addb
Robin Gombkötö <robin.gombkoto@infobric.com>
2025-07-07 13:08:20 +0200

    login/auth endpoints done

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -43,7 +43,39 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    
+    try{
+        const getUser = db.prepare(`
+            SELECT * FROM users WHERE username = ?
+            `);
 
+        // Get user from db and check if there is one associated with username
+        const user = getUser.get(username);
+        if(!user){
+            return res.status(404).send({message: "User was not found."});
+        }
+
+        // Check if password is valid
+        const passwordIsValid = bcrypt.compareSync(password, user.password);
+        if(!passwordIsValid){
+            return res.status(401).send({message: "Invalid password."});
+        } 
+
+        console.log(user);
+
+        const token = jwt.sign(
+            { id: user.id },
+            process.env.JWT_SECRET,
+            { expiresIn: '24h' }
+        )
+
+        res.json({ token });
+    }
+    catch(err){
+        console.log(err.message);
+        res.sendStatus(503);
+    }
 });
 
 export default router
