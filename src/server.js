207f322888bd77597562c3ef88b0e31d5dfc1305
Robin Gombkötö <robin.gombkoto@infobric.com>
2025-07-07 16:02:16 +0200

    Basic Node CRUD server done

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import authRoutes from './routes/authRoutes.js';
 import todoRoutes from './routes/todoRoutes.js';
+import authMiddleware from './middleware/authMiddleware.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -17,7 +18,7 @@ app.use(express.json());
 
 // Routes
 app.use('/auth', authRoutes);
-app.use('/todos', todoRoutes);
+app.use('/todos', authMiddleware, todoRoutes);
 
 // Serves the HTML file from the /public directory 
 // Also tells express to serve all files from the public folder as static assets
